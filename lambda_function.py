be568a43a0a33996b9e44db41f484ae01497a1b5
timjbruce <33874240+timjbruce@users.noreply.github.com>
2020-05-07 09:42:27 -0400

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+import os
+import requests
+import sys
+
+dynamodb = boto3.resource('dynamodb')
+filetable = os.getenv('FileTable', 'CallbackFiles')
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+
+    URI = event['name']
+
+    keyconditionexpression = 'URI = :val1'
+    expressionattributevalues = {':val1':URI}
+    files = dynamodb.query(TableName=filetable, Limit=100,
+        KeyConditionExpression=keyconditionexpression,
+        ExpressionAttributeValues=expressionattributevalues)
+    print(files)
+    for file in files['Items']:
+        shortfile = file.FilenameURL[file.FilenameURL.rfind('/')+1:]
+        fullfile = '/tmp/' + shortfile
+        response = requests.get(file.FilenameURL)
+        if response.code == 200:
+            open(fullfile, 'wb').write(response.content)
+            s3.put_object(Body=fullfile, Bucket='awstb-useast2-datacopy', Key=shortfile)
+            #could delete file in dynamodb
+
+    return {
+        "statusCode": 200,
+        "body": ""
+    }
+    
+def container_start():
+    #create an event and context that is like that passed in from Step Functions
+    event = {}
+    context = {}
+    event['name']=sys.argv[1]
+    #call lambda_handler
+    response = lambda_handler(event, context)
+    return response
